848fa540fd27618455c675fbbb159815ea2efae3
Jacob <meju@gmail.com>
2025-03-16 09:12:15 +0200

    config script

---
+++ infra-Automation/scripts/machineConfig.py
@@ -0,0 +1,37 @@
+import json
+
+# scripts to configure machines
+
+def inputer():
+    while True:
+        machine_list = []
+        machines = ["docker", "kubernetes"]
+        name = str(input("please enter machine name (\"done\" for exit): "))
+        if name.lower() != "done":
+            return machine_list
+        elif name.lower() not in machines:
+            print(f'machine {name} does not exist')
+            continue
+        else:
+            os_list = ["linux", "windows"]
+            os = str(input("please enter the os: "))
+            if os.lower() not in os_list:
+                print("os does not exist, please try again: ")
+                continue
+            cpu = float(input("please enter the CPU: "))
+            ram = float(input("please enter the RAM:"))
+            adding = {"name": name,
+                      "os": os,
+                      "cpu": cpu,
+                      "ram": ram}
+            machine_list.append(adding)
+
+# write it on the file
+
+res = inputer()
+
+with open("users/home/rollingProject/infra-Automation/configs/instances.json", "w") as file:
+    file.write(json.dumps(res))
+    
+print("configuration succesfully saved")
+        
